a160c3c4db950794918f5cd91003bd347714d632
taitrancoding <taitran251105@gmail.com>
2025-11-27 16:30:02 +0700

    update PackageMapper logic

--- aesp/src/main/java/ut/aesp/mapper/PackageMapper.java
+++ aesp/src/main/java/ut/aesp/mapper/PackageMapper.java
@@ -6,11 +6,19 @@ import ut.aesp.dto.packagee.PackageResponse;
 import ut.aesp.dto.packagee.PackageRequest;
 import ut.aesp.model.Package;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import ut.aesp.dto.packagee.PackageRequest;
+import ut.aesp.dto.packagee.PackageResponse;
+import ut.aesp.model.Package;
+
 @Mapper(componentModel = "spring")
 public interface PackageMapper {
 
+  @Mapping(target = "mentors", ignore = true)
   Package toEntity(PackageRequest dto);
 
+  @Mapping(target = "mentors", ignore = true)
   PackageResponse toResponse(Package saved);
 
 }
