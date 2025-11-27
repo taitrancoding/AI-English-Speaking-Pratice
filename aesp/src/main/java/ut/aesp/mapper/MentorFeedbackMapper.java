266f1926354733636a0eeaeb52dbe0df38dcfb5c
taitrancoding <taitran251105@gmail.com>
2025-11-27 16:28:03 +0700

    update MentorFeedbackMapper logic

--- aesp/src/main/java/ut/aesp/mapper/MentorFeedbackMapper.java
+++ aesp/src/main/java/ut/aesp/mapper/MentorFeedbackMapper.java
@@ -2,20 +2,22 @@ package ut.aesp.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import ut.aesp.dto.feedback.MentorFeedbackRequest;
-import ut.aesp.dto.feedback.MentorFeedbackResponse;
+import ut.aesp.dto.mentor.MentorFeedbackRequest;
+import ut.aesp.dto.mentor.MentorFeedbackResponse;
 import ut.aesp.model.MentorFeedback;
 
 @Mapper(componentModel = "spring")
 public interface MentorFeedbackMapper {
-  // @Mapping(target = "mentor.id", source = "mentorId")
-  // @Mapping(target = "learner.id", source = "learnerId")
-  // @Mapping(target = "session.id", source = "sessionId")
+  @Mapping(target = "learner", ignore = true)
+  @Mapping(target = "mentor", ignore = true)
+  @Mapping(target = "practiceSession", ignore = true)
+  @Mapping(target = "feedbackDate", ignore = true)
   MentorFeedback toEntity(MentorFeedbackRequest dto);
 
-  @Mapping(target = "mentorId", source = "mentor.id")
   @Mapping(target = "learnerId", source = "learner.id")
-  @Mapping(target = "sessionId", source = "session.id")
+  @Mapping(target = "learnerName", expression = "java(entity.getLearner().getUser() != null ? entity.getLearner().getUser().getName() : entity.getLearner().getName())")
+  @Mapping(target = "mentorId", source = "mentor.id")
+  @Mapping(target = "mentorName", expression = "java(entity.getMentor().getUser() != null ? entity.getMentor().getUser().getName() : null)")
+  @Mapping(target = "practiceSessionId", expression = "java(entity.getPracticeSession() != null ? entity.getPracticeSession().getId() : null)")
   MentorFeedbackResponse toResponse(MentorFeedback entity);
-
 }
