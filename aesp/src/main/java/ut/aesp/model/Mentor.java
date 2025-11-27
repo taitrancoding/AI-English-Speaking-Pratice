b5faf5b1f4d3cb517e569c80d322086ca06f4426
taitrancoding <taitran251105@gmail.com>
2025-11-27 16:33:30 +0700

    update Mentor entity

--- aesp/src/main/java/ut/aesp/model/Mentor.java
+++ aesp/src/main/java/ut/aesp/model/Mentor.java
@@ -1,12 +1,14 @@
 package ut.aesp.model;
 
-import org.hibernate.envers.Audited;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.envers.Audited;
 import ut.aesp.enums.AvailabilityStatus;
 
 @Getter
@@ -39,4 +41,8 @@ public class Mentor extends Auditable {
   @Enumerated(EnumType.STRING)
   @Column(length = 20)
   private AvailabilityStatus availabilityStatus = AvailabilityStatus.AVAILABLE; // available | busy | inactive
+
+  @JsonIgnore
+  @ManyToMany(mappedBy = "mentors")
+  private Set<Package> packages = new HashSet<>();
 }
